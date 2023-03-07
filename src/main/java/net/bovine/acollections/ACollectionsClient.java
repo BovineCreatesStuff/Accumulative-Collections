56a42f30c57ccacc7cb51fa0d26b49d7186721f6
BovineCreatesStuff <dachemistofx@gmail.com>
2023-03-07 16:15:31 -0700

    First Files Added

---
+++ src/main/java/net/bovine/acollections/ACollectionsClient.java
@@ -0,0 +1,10 @@
+package net.bovine.acollections;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ACollectionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bovine/acollections/ACollectionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bovine.acollections;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ACollectionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
